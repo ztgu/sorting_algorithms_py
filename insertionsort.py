95d3d34f1dc6a0894c7e705e2f8b94d8f4036641
Ashwanth K <97865565+AK16092003@users.noreply.github.com>
2022-08-14 11:26:38 +0530

    Update insertionsort.py
    eliminate swap function

--- insertionsort.py
+++ insertionsort.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python2
 
-def swap(array, i, j):
-    tmp = array[i]
-    array[i] = array[j]
-    array[j] = tmp
-
 def insertionsort(array):
     for i in range(0, len(array)):
         j = i
         while j > 0 and array[j-1] > array[j]:
-            swap(array, j-1, j)
+            array[j-1], array[j] = array[j] , array[j-1]
             j -= 1
 
 if __name__ == "__main__":
